66b2612a56ccbff8d1ce074d939f216e16d29ac7
Felipe Gomez <felipegh@outlook.com>
2021-07-24 11:24:45 -0500

    Dart examples removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
-import 'src/app.dart';
+import 'package:flutter_basic_examples/src/pages/home.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Components App',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
 }
